41124c460d7b6db8ae33d8e801d3dbe7fbcae164
narasimhanaik123321-sys <narasimhanaik123321@gmail.com>
2026-03-19 13:52:02 +0530

    Update Student.js

--- server/models/Student.js
+++ server/models/Student.js
@@ -1,9 +1,21 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
+
+// const StudentSchema = new mongoose.Schema({
+//   name: String,
+//   email: String,
+//   age: Number
+// })
+
+// module.exports = mongoose.model("Student", StudentSchema)
+
+
+
+const mongoose = require("mongoose")
 
 const StudentSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  age: Number
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  age: { type: Number, required: true }
 })
 
 module.exports = mongoose.model("Student", StudentSchema)
